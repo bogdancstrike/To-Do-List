29f79af065937070089758a32fb4076f3076fca5
Bogdan Doncea <bogdan.cstrike@yahoo.com>
2021-06-19 20:53:11 +0300

    to-do-list made with react and json server

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+//rafce
+
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright Doncea Bogdan &copy; 2021</p>
+      <Link to="/about">About</Link>
+    </footer>
+  );
+};
+
+export default Footer;
